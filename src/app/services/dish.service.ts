b001a1d7747d000093d12b2bec7de4afdf31da15
shriyakabra97 <shriyakabra97@gmail.com>
2020-08-04 20:56:05 +0530

    Single Page Application Part 2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -13,7 +13,7 @@ export class DishService {
     return DISHES ;
   }
   getDish(id: string) : Dish{
-      return DISHES.filter((dish) => { dish.id === id})[0];
+      return DISHES.filter((dish) => (dish.id === id))[0];
   }
   getFeaturedDish() : Dish{
       return DISHES.filter((dish) => dish.featured)[0];
