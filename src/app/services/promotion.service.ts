bb3f1db433f83876bb8f2c99782d167acb2103d3
shriyakabra97 <shriyakabra97@gmail.com>
2020-08-19 09:17:43 +0530

    Promise Part 2

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Promotion } from '../shared/promotion';
 import { PROMOTIONS } from '../shared/promotions';
+import { resolve } from 'url';
+import { DISHES } from '../shared/dishes';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,12 +11,19 @@ export class PromotionService {
   constructor() { }
 
   getPromotions() : Promise<Promotion[]> {
-    return Promise.resolve(PROMOTIONS) ;
+    return new Promise( resolve => {
+      setTimeout(() => resolve(PROMOTIONS), 2000);
+    });
   }
   getPromotion(id: string) : Promise<Promotion>{
-      return Promise.resolve(PROMOTIONS.filter((promo) => { promo.id === id})[0]);
+    return new Promise(resolve=> {
+      // Simulate server latency with 2 second delay
+        setTimeout(() => resolve(PROMOTIONS.filter((promotion) => (promotion.id === id))[0]), 2000);
+    });
   }
   getFeaturedPromotion() : Promise<Promotion>{
-      return Promise.resolve(PROMOTIONS.filter((promo) => promo.featured)[0]);
-  }
+    return  new Promise(resolve=> {
+      // Simulate server latency with 2 second delay
+        setTimeout(() => resolve(PROMOTIONS.filter((promotion) => promotion.featured)[0]), 2000);
+    });  }
 }
