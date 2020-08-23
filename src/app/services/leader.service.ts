0fed44646e19c6e33e031925994e8394fd5357ca
shriyakabra97 <shriyakabra97@gmail.com>
2020-08-23 19:43:53 +0530

    RxJS Part 1

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
+import { of, Observable } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +11,23 @@ export class LeaderService {
 
   constructor() { }
 
-  getLeaders(): Promise<Leader[]> {
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(LEADERS), 2000);
-    });  }
-  getFeaturedLeader() : Promise<Leader>{
-    return  new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(LEADERS.filter((leader) => leader.featured)[0]), 2000);
-    });  }
+  // getLeaders(): Promise<Leader[]> {
+  //   return new Promise(resolve=> {
+  //     // Simulate server latency with 2 second delay
+  //       setTimeout(() => resolve(LEADERS), 2000);
+  //   });  
+  // }
+  // getFeaturedLeader() : Promise<Leader>{
+  //   return  new Promise(resolve=> {
+  //     // Simulate server latency with 2 second delay
+  //       setTimeout(() => resolve(LEADERS.filter((leader) => leader.featured)[0]), 2000);
+  //   }); 
+//  }
+
+getLeaders(): Observable<Leader[]> {
+   return of(LEADERS).pipe(delay(2000));
+}
+getFeaturedLeader() : Observable<Leader>{
+  return of(LEADERS.filter((leader) => leader.featured)[0]).pipe(delay(2000));
+ }
 }
--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -3,6 +3,8 @@ import { Promotion } from '../shared/promotion';
 import { PROMOTIONS } from '../shared/promotions';
 import { resolve } from 'url';
 import { DISHES } from '../shared/dishes';
+import { of, Observable } from 'rxjs';
+import { delay } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -10,20 +12,33 @@ export class PromotionService {
 
   constructor() { }
 
-  getPromotions() : Promise<Promotion[]> {
-    return new Promise( resolve => {
-      setTimeout(() => resolve(PROMOTIONS), 2000);
-    });
+  // getPromotions() : Promise<Promotion[]> {
+  //   return new Promise( resolve => {
+  //     setTimeout(() => resolve(PROMOTIONS), 2000);
+  //   });
+  // }
+  // getPromotion(id: string) : Promise<Promotion>{
+  //   return new Promise(resolve=> {
+  //     // Simulate server latency with 2 second delay
+  //       setTimeout(() => resolve(PROMOTIONS.filter((promotion) => (promotion.id === id))[0]), 2000);
+  //   });
+  // }
+  // getFeaturedPromotion() : Promise<Promotion>{
+  //   return  new Promise(resolve=> {
+  //     // Simulate server latency with 2 second delay
+  //       setTimeout(() => resolve(PROMOTIONS.filter((promotion) => promotion.featured)[0]), 2000);
+  //   });  
+  // }
+
+
+  getPromotions() : Observable<Promotion[]> {
+    return of(PROMOTIONS).pipe(delay(2000));
+
   }
-  getPromotion(id: string) : Promise<Promotion>{
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(PROMOTIONS.filter((promotion) => (promotion.id === id))[0]), 2000);
-    });
+  getPromotion(id: string) : Observable<Promotion>{
+    return of(PROMOTIONS.filter((promotion) => (promotion.id === id))[0]).pipe(delay(2000));
   }
-  getFeaturedPromotion() : Promise<Promotion>{
-    return  new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(PROMOTIONS.filter((promotion) => promotion.featured)[0]), 2000);
-    });  }
+  getFeaturedPromotion() : Observable<Promotion>{
+    return of(PROMOTIONS.filter((promotion) => promotion.featured)[0]).pipe(delay(2000));
+  }
 }
